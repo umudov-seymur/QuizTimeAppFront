cd281e37397b39459f17564369c066266609d9d8
Seymur Umudov <u.seymur@outlook.com>
2022-03-06 21:25:33 +0400

    update fixed header

--- src/App.vue
+++ src/App.vue
@@ -3,17 +3,19 @@
     <div class="main flex h-screen bg-gray-50 dark:bg-gray-900">
       <div class="flex flex-col flex-1 w-full z-10">
         <AppHeader />
-        <transition
-          enter-active-class="transition ease-out duration-150"
-          enter-class="transform opacity-0 scale-95"
-          enter-to-class="transform opacity-100 scale-100"
-          leave-active-class="transition ease-in duration-100"
-          leave-class="transform opacity-100 scale-100"
-          leave-to-class="transform opacity-0 scale-95"
-          mode="out-in"
-        >
-          <router-view />
-        </transition>
+        <main class="h-full overflow-y-auto">
+          <transition
+            enter-active-class="transition ease-out duration-150"
+            enter-class="transform opacity-0 scale-95"
+            enter-to-class="transform opacity-100 scale-100"
+            leave-active-class="transition ease-in duration-100"
+            leave-class="transform opacity-100 scale-100"
+            leave-to-class="transform opacity-0 scale-95"
+            mode="out-in"
+          >
+            <router-view />
+          </transition>
+        </main>
         <AppFooter />
       </div>
     </div>
