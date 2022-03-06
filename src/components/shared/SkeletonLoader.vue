7e3b9d64b08bf05d868e0e2d69dd0e0a53583fdd
Seymur Umudov <u.seymur@outlook.com>
2022-03-06 21:34:28 +0400

    feat: added skeleton loader component

--- src/components/shared/SkeletonLoader.vue
+++ src/components/shared/SkeletonLoader.vue
@@ -1,11 +1,17 @@
 <template>
   <div class="animate-pulse">
-    <div class="bg-gray-300 h-6 rounded-md"></div>
+    <div class="h-6 rounded-md" :class="styles"></div>
   </div>
 </template>
 
 <script>
 export default {
   name: "SkeletonLoader",
+  props: {
+    styles: {
+      type: Array,
+      default: () => ['bg-gray-400'],
+    },
+  },
 };
 </script>
