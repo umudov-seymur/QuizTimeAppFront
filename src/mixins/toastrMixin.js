bedbfc378d9358536656b504cfa33288a61a6997
Seymur Umudov <u.seymur@outlook.com>
2022-03-13 20:54:54 +0400

    feat: added swal confirmation

--- src/mixins/toastrMixin.js
+++ src/mixins/toastrMixin.js
@@ -13,5 +13,20 @@ Vue.mixin({
         timerProgressBar: true,
       });
     },
+    confirmationDelete(callback) {
+      this.$swal({
+        title: this.$t("Are you sure?"),
+        text: this.$t("You won't be able to revert this!"),
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: this.$t("Yes, delete it!"),
+        cancelButtonText: this.$t("Cancel"),
+      }).then((result) => {
+        if (!result.isConfirmed) return;
+        callback();
+      });
+    },
   },
 });
